db19deceb732113bdac311fa4d50692f2bd7b83d
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-01-05 11:40:50 +0100

    add folder for data input

---
+++ Load_Data/cmake-build-debug/CMakeFiles/load_numpy_c__.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for load_numpy_c__.
+# This may be replaced when dependencies are built.
---
+++ Load_Data/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This code is used to read numpy arrays into C++ code!" << std::endl;
+    return 0;
+}
