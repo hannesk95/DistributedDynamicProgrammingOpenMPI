a08f750036b970657df9439e076e8e07aa68534d
papstchaka <alex.christoph.ac@gmail.com>
2021-01-06 22:10:42 +0100

    added example how to imread .npy files with C++

---
+++ data_import/README.md
@@ -0,0 +1,3 @@
+# `data_debug`
+
+contains with <a href="main.cpp" target="_blank">`main.cpp`</a> an example how to imread `.npy`-files directly into C++
---
+++ data_import/cmake-build-debug/CMakeFiles/data_import.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_import.dir/main.cpp.o"
+  "data_import"
+  "data_import.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_import.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
