6441d723c3a8ec6ecab9fdade0fd2556d9543712
ge37bov <daniel@DESKTOP-1RNLTVN.localdomain>
2021-01-10 18:55:46 +0100

    Added another data folder and MSE computation in main

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "vi_processor_impl_local.h"
 #include "vi_processor_impl_distr_01.h"
 
+#include "cnpy.h"
+
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
@@ -22,6 +24,16 @@ int main(int argc, char *argv[])
         .Param_npz_dict_filename    = "../data_debug/parameters.npz"
     };
 
+    cnpy::NpyArray raw_J_star = cnpy::npy_load("../data_debug/J_star_alpha_0_99_iter_1000.npy");
+    cnpy::NpyArray raw_Pi_star = cnpy::npy_load("../data_debug/pi_star_alpha_0_99_iter_1000.npy");
+
+    std::vector<float> J_star_vec = raw_J_star.as_vec<float>();
+    std::vector<int> Pi_star_vec = raw_Pi_star.as_vec<int>();
+
+    Eigen::Map<Eigen::VectorXf> J_star(J_star_vec.data(), J_star_vec.size());
+    Eigen::Map<Eigen::VectorXi> Pi_star(Pi_star_vec.data(), Pi_star_vec.size());
+
+
     std::vector<std::unique_ptr<VI_Processor_Base>> processors;
     for(const int& comm_period : {10,50,100,500})
         processors.push_back(std::unique_ptr<VI_Processor_Base>(new VI_Processor_Impl_Distr_01(args, comm_period)));
@@ -30,6 +42,7 @@ int main(int argc, char *argv[])
     const int n_runs = 20;
 
     Eigen::MatrixXf measurements(n_runs, processors.size());
+    Eigen::VectorXf mse_J(processors.size());
 
     for(int i=0; i<n_runs; i++)
     {
@@ -43,17 +56,26 @@ int main(int argc, char *argv[])
 
             measurements.coeffRef(i,j) = time_in_sec;
 
-            //std::unique_ptr<Eigen::VectorXi> Pi;
-            //std::unique_ptr<Eigen::VectorXf> J;
-            //(processors[i].get())->GetPiAndJ(Pi, J);
+            std::unique_ptr<Eigen::VectorXi> Pi;
+            std::unique_ptr<Eigen::VectorXf> J;
+            (processors[i].get())->GetPiAndJ(Pi, J);
+
+            auto J_se = (*J.get() - J_star).cwiseAbs2();
+            mse_J.coeffRef(j) = J_se.mean();
         }
     }
 
     if(0 == world_rank)
     {
         // Print mean execution time for each processor implementation
+        std::cout << "========= Mean execution times =========" << std::endl;
         Eigen::VectorXf t_mean =  measurements.colwise().mean();
         std::cout << t_mean << std::endl;
+
+        
+        // Print mean squared error of J for each processor implementation
+        std::cout << "========= MSE's of J vectors =========" << std::endl;
+        std::cout << mse_J << std::endl;
     }
 
     MPI_Finalize();
