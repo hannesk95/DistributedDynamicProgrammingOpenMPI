abb48c93ea575a4346ba691f000f61dad147d5dd
papstchaka <alex.christoph.ac@gmail.com>
2021-01-13 16:00:56 +0100

    updated DEF at the start to define right file

--- src/vi_processor_impl_distr_02.h
+++ src/vi_processor_impl_distr_02.h
@@ -1,5 +1,5 @@
-#ifndef __VI_PROCESSOR_IMPL_DISTR_H
-#define __VI_PROCESSOR_IMPL_DISTR_H
+#ifndef __VI_PROCESSOR_IMPL_DISTR_02_H
+#define __VI_PROCESSOR_IMPL_DISTR_02_H
 
 #include "vi_processor_base.h"
 
