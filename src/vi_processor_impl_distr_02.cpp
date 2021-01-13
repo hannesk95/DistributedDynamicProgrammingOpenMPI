1148cec0cfe88a61d213f00810a07177a79e5124
papstchaka <alex.christoph.ac@gmail.com>
2021-01-13 15:09:49 +0100

    refactored send/recv - working now!

--- src/vi_processor_impl_distr_02.cpp
+++ src/vi_processor_impl_distr_02.cpp
@@ -18,13 +18,17 @@ void VI_Processor_Impl_Distr_02::value_iteration_impl(
     const int process_first_state = (J.size() / world_size) * world_rank;
     const int process_last_state =  (world_size == world_rank + 1) ? J.size() : (J.size() / world_size) * (world_rank + 1);
 
-    // define variables that root needs
-    const int last_state = world_size - 1;
-    const int workload = (J.size() / world_size);
-    const int last_workload = J.size() - (workload * last_state);
-    const int sub_size = J.size() / world_size;
-    Eigen::VectorXf J_new(J.size());
-    J_new.fill(0);
+    // same received J and Pi
+    std::vector<float> J_recv_buffer(0);
+    std::vector<int> Pi_recv_buffer(0);
+    const int remainder = (J.size() % world_size);
+    const int buffer_size = (J.size() / world_size);
+    // root, only it receives data
+    if (world_rank == root_id)
+    {
+        J_recv_buffer.resize(buffer_size + remainder);
+        Pi_recv_buffer.resize(buffer_size + remainder);
+    }
 
     // Keeps track of the change in J vector
     float error = 0;
@@ -40,73 +44,86 @@ void VI_Processor_Impl_Distr_02::value_iteration_impl(
         // Exchange results every comm_period cycles
         if(t % comm_period == 0)
         {
-            float* J_raw = J.data();
-            MPI_Send(&J_raw, process_last_state - process_first_state, MPI_FLOAT, root_id, 0, MPI_COMM_WORLD);
-            if (world_rank == 0)
+            // all non-roots are sending to root
+            if (world_rank != 0)
             {
-                Eigen::VectorXf J_raw(sub_size);
-                // Synchronize all workers
-                MPI_Barrier(MPI_COMM_WORLD);
-                // set first piece
-                J_new.segment(0, workload) = J.segment(0, workload);
-                for (int i = 0; i < world_size; i++)
+                float* J_raw = J.data();
+                MPI_Send(J_raw + process_first_state, process_last_state - process_first_state, MPI_FLOAT,  root_id, 0, MPI_COMM_WORLD); // MPI_ANY_TAG is only valid for recv
+            }
+            // root receives
+            else
+            {
+                // receive from all except root
+                for (int i = 0; i < world_size - 1; ++i)
                 {
-                  if (i < last_state)
+                  // find out who is sending! Wait for the first message that occurs
+                  MPI_Status status;
+                  MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                  int source_id = status.MPI_SOURCE;
+                  // get payload size
+                  auto payload_size = buffer_size;
+                  // last one has a little bit more payload!
+                  if (source_id == world_size - 1)
                   {
-                      MPI_Recv(&J_raw, workload, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                      J_new.segment(workload * i, workload) = J_raw;
+                      payload_size += remainder;
                   }
-                  else
-                  {
-                      MPI_Recv(&J_raw, last_workload, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                      J_new.segment(workload * last_state, last_workload) = J_raw;
-                  }
-                }
-
-                // Check for convergence
-                Eigen::Map<Eigen::VectorXf> J_recv(J_new.data(), J_new.size());
-                auto J_abs_diff = (J - J_recv).cwiseAbs();
-                error = J_abs_diff.maxCoeff();
-
-                // Overwrite J
-                J = J_new;
-
+                  // now receive the sub vector into J receive buffer
+                  MPI_Recv(J_recv_buffer.data(), payload_size, MPI_FLOAT, source_id, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                  // Generate Eigen style vector of received data
+                  Eigen::Map<Eigen::VectorXf> J_recv(J_recv_buffer.data(), payload_size);
+                  // get difference between J_recv and corresponding segment in J
+                  float recv_max_diff = (J.segment(source_id * buffer_size, payload_size) - J_recv).cwiseAbs().maxCoeff();
+                  // check if new maximum difference -> if yes overwrite
+                  if (recv_max_diff > error) error = recv_max_diff;
+                  // update J
+                  J.segment(source_id * buffer_size, payload_size) = J_recv;
+                } // root has now maximum error value and new J vector
+                // Broadcast error from root to all processes (that they can stop respectively), also synchronizes all processes
+                MPI_Bcast(&error, 1, MPI_FLOAT, root_id, MPI_COMM_WORLD);
                 // If convergence rate is below threshold stop
                 if(error <= e_max)
                 {
                     debug_message("Converged after " + std::to_string(t) + " iterations with communication period " + std::to_string(comm_period));
                     break;
                 }
-                MPI_Bcast(J_new.data(), J_new.size(), MPI_FLOAT, root_id, MPI_COMM_WORLD);
-
+                // Broadcast J again from root to all processes because we did not converge yet
+                MPI_Bcast(J.data(), J.size(), MPI_FLOAT, root_id, MPI_COMM_WORLD);
                 // Reset change rate to 0 such that it can be overwritten with a new (smaller) value
                 error = 0;
             }
         }
-
     }
+    // now merge policy accordingly to J
 
-    std::vector<int> recvcounts;    // Number of states for each process
-    std::vector<int> displs;        // Displacement of sub vectors for each process
-    for(int i=0; i < world_size; ++i)
+    // all non-roots are sending to root
+    if (world_rank != 0)
     {
-        if(i+1 < world_size) recvcounts.push_back((J.size() / world_size));
-        else recvcounts.push_back((J.size() / world_size) + J.size() % world_size);
-
-        if(i == root_id) displs.push_back(0);
-        else displs.push_back(displs[i-1] + recvcounts[i-1]);
+        int* Pi_raw = Pi.data();
+        MPI_Send(Pi_raw + process_first_state, process_last_state - process_first_state, MPI_INT, root_id, 0, MPI_COMM_WORLD); // MPI_ANY_TAG is only valid for recv
     }
-
-    // Merge Policy
-    int* Pi_raw = Pi.data();
-    MPI_Gatherv(&Pi_raw[process_first_state],
-                process_last_state - process_first_state,
-                MPI_INT,
-                Pi_raw,
-                recvcounts.data(),
-                displs.data(),
-                MPI_INT,
-                root_id,
-                MPI_COMM_WORLD
-    );
+    // root receives
+    else
+    {
+        // receive from all except root
+        for (int i = 0; i < world_size - 1; ++i)
+        {
+            // find out who is sending! Wait for the first message that occurs
+            MPI_Status status;
+            MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            int source_id = status.MPI_SOURCE;
+            // get payload size
+            auto payload_size = buffer_size;
+            // last one has a little bit more payload!
+            if (source_id == world_size - 1)
+            {
+                payload_size += remainder;
+            }
+            // now receive the sub vector into Pi receive buffer
+            MPI_Recv(Pi_recv_buffer.data(), payload_size, MPI_INT, source_id, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // Generate Eigen style vector of received data
+            Eigen::Map<Eigen::VectorXi> Pi_recv(Pi_recv_buffer.data(), payload_size);
+            // update Pi
+            Pi.segment(source_id * buffer_size, payload_size) = Pi_recv;
+        }
+    }
 }
