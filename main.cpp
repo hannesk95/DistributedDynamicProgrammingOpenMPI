66498c15a85d97a9beb66b54b85cbbecb3988030
papstchaka <alex.christoph.ac@gmail.com>
2021-01-14 20:21:49 +0100

    reversed previous changes

--- main.cpp
+++ main.cpp
@@ -40,9 +40,9 @@ int main(int argc, char *argv[])
     Eigen::Map<Eigen::VectorXi> Pi_star(Pi_star_vec.data(), Pi_star_vec.size());
 
     std::vector<std::unique_ptr<VI_Processor_Base>> processors;
-    for(const int& comm_period : periods)
+    for(const int& comm_period : comm_periods)
         processors.push_back(std::unique_ptr<VI_Processor_Base>(new VI_Processor_Impl_Distr_01(args, 0, comm_period)));
-    for(const int& comm_period : periods)
+    for(const int& comm_period : comm_periods)
         processors.push_back(std::unique_ptr<VI_Processor_Base>(new VI_Processor_Impl_Distr_02(args, 0, comm_period)));
     processors.push_back(std::unique_ptr<VI_Processor_Base>(new VI_Processor_Impl_Distr_42(args, 0)));
     processors.push_back(std::unique_ptr<VI_Processor_Base>(new VI_Processor_Impl_Local(args, 0)));
