8bab537a3232d24f623a9187c4bc7235c2d4d46a
ge37bov <daniel@DESKTOP-1RNLTVN.localdomain>
2021-01-14 21:31:48 +0100

    Forgot one conflict...

--- data/convert_pickle.py
+++ data/convert_pickle.py
@@ -9,9 +9,4 @@ for name in os.listdir(os.getcwd()):
         with open(os.path.join(path,"parameters.pickle"), "rb") as the_file:
             parameters = pickle.load(the_file)
             
-<<<<<<< HEAD
         np.savez(os.path.join(path,"parameters.npz"), **parameters)
-=======
-        np.savez(os.path.join(path,"parameters.npz"), **parameters)
-        
->>>>>>> master
