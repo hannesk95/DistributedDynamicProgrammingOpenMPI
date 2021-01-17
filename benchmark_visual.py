42d78ea7a2d8e1f8b0c1e23a78eaa0d9d7010648
Hannes <hannes.kiechle@gmail.com>
2021-01-17 23:40:24 +0100

    update visualization script

--- benchmark_visual.py
+++ benchmark_visual.py
@@ -2,10 +2,6 @@ import os
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
-#import seaborn as sns
-#%matplotlib inline
-#import matplotlib.pyplot as plt
-#plt.style.use('seaborn-whitegrid')
 
 def load_data(path):
 
@@ -33,8 +29,8 @@ def autolabel(ax, rects):
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),
                     textcoords="offset points",
-                    ha='center', va='bottom', fontsize=16)        
-
+                    ha='center', va='bottom', fontsize=10)
+        
 def plot_benchmark_distr(data):   
 
     times = {}
@@ -42,7 +38,8 @@ def plot_benchmark_distr(data):
     labels = []
     
     for label in list(data.keys()):
-        result = label.split("-")       
+        result = label.split("-")
+        
         
         if any(x in label for x in ["Local", "local"]):
             continue
@@ -64,34 +61,39 @@ def plot_benchmark_distr(data):
         times[scheme][freq] = data[label]['mean_execution_time'][0]
         
     x = np.arange(len(comm))
-    width = 0.35
+    width = 0.1
     
     fig, ax = plt.subplots(figsize=(16,8))
     plt.rcParams['xtick.labelsize']=16
     plt.rcParams['ytick.labelsize']=16
-    rects1 = ax.bar(x - width/2, times['01'].values(), width, label='Scheme: 01')
-    rects2 = ax.bar(x + width/2, times['02'].values(), width, label='Scheme: 02')
+    rects1 = ax.bar(x - 0.25, times[list(times.keys())[0]].values(), width, label='Scheme: ' + list(times.keys())[0])
+    rects2 = ax.bar(x + 0.1, times[list(times.keys())[1]].values(), width, label='Scheme: ' + list(times.keys())[1])
+    rects3 = ax.bar(x - 0.1, times[list(times.keys())[2]].values(), width, label='Scheme: ' + list(times.keys())[2])
+    rects4 = ax.bar(x + 0.25, times[list(times.keys())[3]].values(), width, label='Scheme: ' + list(times.keys())[3])
     
     ax.set_ylabel('Mean execution time (in seconds)', fontsize=16)
     ax.set_title('Benchmark comparison among communication schemes', fontsize=20)
     ax.set_xticks(x)
     ax.set_xticklabels(comm)
-    ax.set_xlabel('Communication/update frequency (in epochs)', fontsize=16)
-    ax.legend(fontsize='x-large')    
+    ax.set_xlabel('Communication frequency (in epochs)', fontsize=16)
+    ax.legend(fontsize='x-large')
+    
     
     autolabel(ax, rects1)
     autolabel(ax, rects2)
+    autolabel(ax, rects3)
+    autolabel(ax, rects4)
     
     fig.tight_layout()
     plt.grid()
     plt.savefig('./results/benchmark_distr.png')
-    #plt.show()        
-
+    plt.show()    
+    
 def main():
     
     result_dir = "results"
     data = load_data(os.path.join(os.getcwd(), result_dir))
-    plot_benchmark_distr(data)    
-
-if __name__ == "__main__":  
+    plot_benchmark_distr(data)
+    
+if __name__ == "__main__":
     main()
